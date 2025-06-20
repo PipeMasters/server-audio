d4274e50f3becb67d60dac67f36de9e1c8191610
yobange <recum12233@mail.ru>
2025-06-20 20:52:56 +0300

    Initial commit

---
+++ src/main/java/com/pipemasters/serveraudio/ServerAudioApplication.java
@@ -0,0 +1,13 @@
+package com.pipemasters.serveraudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerAudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pipemasters/serveraudio/ServerAudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipemasters.serveraudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerAudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
