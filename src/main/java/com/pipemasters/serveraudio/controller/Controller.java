f72b820dec1b754ebec1d56d30cb4a680f4cd6eb
yobange <recum12233@mail.ru>
2025-07-16 17:46:42 +0300

    test

--- src/main/java/com/pipemasters/serveraudio/controller/Controller.java
+++ src/main/java/com/pipemasters/serveraudio/controller/Controller.java
@@ -4,17 +4,16 @@ import com.pipemasters.serveraudio.service.impl.AudioServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/")
-public class Сontroller {
+public class Controller {
     private final AudioServiceImpl audioService;
 
-    public Сontroller(AudioServiceImpl audioService) {
+    public Controller(AudioServiceImpl audioService) {
         this.audioService = audioService;
     }
 
